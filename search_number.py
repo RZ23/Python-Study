9b303aeef47875be56f82c9ac98c2243df038d32
RZ23 <rz_jiang@yahoo.com>
2022-02-02 09:29:37 -0700

    Add Binary Search Function

--- search_number.py
+++ search_number.py
@@ -84,6 +84,25 @@ def linear_Search_located_number(cards,target_number):
     if index == len(cards):
         return -1
 # print(located_number(tests[5]['input']['cards'],tests[5]['input']['query']))
+def Binary_search_located_number(cards,target_number):
+    l, h = 0,len(cards)-1
+    while(l<=h):
+        mid = int((h+l)/2)
+        if cards[mid]==target_number:
+            if mid -1 >0 and cards[mid-1] ==target_number:
+                h = mid-1
+            else:
+                return mid
+        elif cards[mid]<target_number:
+            h=mid-1
+        elif cards[mid]>target_number:
+            l = mid+1
+        else:
+            return -1
+    return -1
+# print(tests[4])
+# print(Binary_search_located_number(tests[4]['input']['cards'],tests[4]['input']['query']))
+print("Start Linear Search")
 start_time =time.time()
 for test in tests:
         print(test['input']['cards'])
@@ -93,5 +112,17 @@ for test in tests:
         elif calc_out == test['output']:
             print("found "+str(test['input']['query'])+", index is "+str(calc_out))
 end_time = time.time()
-print("runnint time is "+str((end_time-start_time))+"ms")
-
+print("Linear Search finished!")
+print("Linear Search running time is "+str(end_time-start_time)+"ms")
+print("Start Binary Search")
+start_time =time.time()
+for test in tests:
+        print(test['input']['cards'])
+        calc_out=Binary_search_located_number(test['input']['cards'],test['input']['query'])
+        if calc_out==-1:
+            print("Not found "+str(test['input']['query']))
+        elif calc_out == test['output']:
+            print("found "+str(test['input']['query'])+", index is "+str(calc_out))
+end_time = time.time()
+print("Binary Search finished!")
+print("Binary Search running time is "+str(end_time-start_time)+"ms")
