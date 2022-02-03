fe902e756fcc5a2e8f895c6f5513af7810c2c678
RZ23 <rz_jiang@yahoo.com>
2022-02-03 15:19:59 -0700

    add inorder/preorder/postorder function
    Add In-Order, Pre-Order, and Pre-Order functions
    Add Tree to trurple Function

--- Binary_Tree.py
+++ Binary_Tree.py
@@ -17,6 +17,14 @@ def parse_tuple (data):
         node = TreeNode(data)
     return node
 
+# create the turple based on the tree
+def parse_node_to_turple(node):
+    if node is None:
+        return None
+    if node.left is None and node.right is None:
+        return node.key
+    return (parse_node_to_turple(node.left),node.key,parse_node_to_turple(node.right))
+
 tree2 = parse_tuple(tree_tuple)
 print(tree2.key)
 # inorder travel the tree
@@ -24,10 +32,12 @@ def inOrder(node):
     if node is None:
         return[]
     return (inOrder(node.left)+[node.key]+inOrder(node.right))
+# Preorder travel the tree
 def PreOrder(node):
     if node is None:
         return []
     return([node.key]+PreOrder(node.left)+PreOrder(node.right))
+# Postorder travel the tree
 def PostOrder(node):
     if node is None:
          return []
@@ -38,12 +48,5 @@ print("PreOrder Traverse,",end = " ")
 print(PreOrder(tree2))
 print("PostOrder Traverse,",end = " ")
 print(PostOrder(tree2))
-# test2 = parse_tuple((4,2,6),1,(3))
-# print(test2)
-root = TreeNode(1)
-root.left = TreeNode(2)
-root.right = TreeNode(3)
-root.left.left = TreeNode(4)
-root.left.right = TreeNode(5)
-print("PostOrder Traverse,",end = " ")
-print(PostOrder(root))
+
+print(parse_node_to_turple(tree2))
