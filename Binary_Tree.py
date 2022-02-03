70f733e7dc3546897896d6e712ba3e755d4cea9b
RZ23 <rz_jiang@yahoo.com>
2022-02-03 15:47:08 -0700

    Add Calculate Height and Node Function
    Add Calculate Height and Node Function

--- Binary_Tree.py
+++ Binary_Tree.py
@@ -3,8 +3,7 @@ class TreeNode:
         self.key = key
         self.left = None
         self.right = None
-# node1=TreeNode(1)
-# print(node1.key)
+
 tree_tuple = ((1,3,None),2,((None,3,4),5,(6,7,8)))
 def parse_tuple (data):
     if isinstance(data,tuple) and len(data)==3:
@@ -42,11 +41,29 @@ def PostOrder(node):
     if node is None:
          return []
     return(PostOrder(node.left)+PostOrder(node.right)+[node.key])
+
+# Calculate the height of the tree
+def tree_height(node):
+    if node is None:
+        return 0
+    else:
+        return 1 +max(tree_height(node.left),tree_height(node.right))
+#Calculate the node of a tree
+def tree_node(tree):
+    if tree is None:
+        return 0
+    else:
+        return 1+tree_node(tree.left)+tree_node(tree.right)
+
 print("InOrder Traverse,",end = " ")
 print(inOrder(tree2))
 print("PreOrder Traverse,",end = " ")
 print(PreOrder(tree2))
 print("PostOrder Traverse,",end = " ")
 print(PostOrder(tree2))
-
+print("Parse to turple:", end=" ")
 print(parse_node_to_turple(tree2))
+print("Calculate the Height of a tree",end = " ")
+print(tree_height(tree2))
+print("Calculate the nodes of a tree",end = " ")
+print(tree_node(tree2))
