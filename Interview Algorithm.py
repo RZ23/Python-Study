0190e8e82bd1a601dc2f9ab4a2162a941b4364f0
RZ23 <rz_jiang@yahoo.com>
2022-03-02 16:06:31 -0700

    Add Symmetric Tree Check and Gas-Station Problem
    Add Symmetric Tree Check and Gas-Station Problem

--- Interview Algorithm.py
+++ Interview Algorithm.py
@@ -163,3 +163,120 @@ print("With Sorting")
 def Kth_Element_Sorting(s,k):
     return sorted(s)[-k]
 print("the list is {} and the {}th larget element is {}".format(s1,k,Kth_Element_Sorting(s1,k)))
+
+print("------------------Symmetric Tree-----------------------------")
+# Symmetric Tree
+def Symmetric_Tree(root1,root2):
+    if root1 is None and root2 is None:
+        return True
+    elif ((root1 is None) != (root2 is None)) or root1.value!=root2.value:
+        return False
+    return Symmetric_Tree(root1.left,root2.right) and Symmetric_Tree(root2.left,root1.right)
+def Check_Symmetric_Tree(node):
+    if node is None:
+        return True
+    return Symmetric_Tree(node.left,node.right)
+
+class Node():
+    def __init__(self,value=None,left= None,right=None):
+        self.value = value
+        self.right = right
+        self.left=left
+
+node1 = Node(3)
+node2 = Node(0)
+node3 = Node(6)
+node4 = Node(9,node1,node2)
+node5 = Node(7,None,node3)
+node6 = Node(2,node4,node5)
+node7 = Node(1)
+node8 = Node(8,node7,None)
+node9 = Node(5,node6,node8)
+node10 = Node(1)
+node11 = Node(8,None,node10)
+node12 = Node(6)
+node13 = Node(0)
+node14 = Node(3)
+node15 = Node(7,node12,None)
+node16 = Node(9,node13,node14)
+node17 = Node(2,node15,node16)
+node18 =Node(5,node11,node17)
+Symmetric_Head = Node(10,node9,node18)
+# Node_List = [Symmetric_Head,node1,node2,node3,node4,node5,node6,node7,node8,node9,
+#              node10,node11,node12,node13,node14,node15,node16,node17, node18,node18]
+#
+# for item in Node_List:
+#     if item.left is None and item.right is None:
+#         print("Node {}'s Left Node is {} and the right Node is {}".format(item.value, None,None))
+#     elif item.right is None:
+#         print("Node {}'s Left Node is {} and the right Node is {}".format(item.value, item.left.value, None))
+#     elif item.left is None:
+#         print("Node {}'s Left Node is {} and the right Node is {}".format(item.value, None, item.right.value))
+#     else:
+#         print("Node {}'s Left Node is {} and the right Node is {}".format(item.value, item.left.value,item.right.value) )
+print("The Tree is the symmetric Tree? {}".format(Check_Symmetric_Tree(Symmetric_Head)))
+Un_node1 = Node(3)
+Un_node2 = Node(0)
+Un_node3 = Node(6)
+Un_node4 = Node(9,Un_node1,Un_node2)
+Un_node5 = Node(7,None,Un_node3)
+Un_node6 = Node(2,Un_node4,Un_node5)
+Un_node7 = Node(1)
+Un_node8 = Node(8,Un_node7,None)
+# Change left and right child to creat teh un-symmetric Tree
+Un_node9 = Node(5,Un_node8,Un_node6)
+Un_node10 = Node(1)
+Un_node11 = Node(8,None,Un_node10)
+Un_node12 = Node(6)
+Un_node13 = Node(0)
+Un_node14 = Node(3)
+Un_node15 = Node(7,Un_node12,None)
+Un_node16 = Node(9,Un_node13,Un_node14)
+Un_node17 = Node(2,Un_node15,Un_node16)
+Un_node18 =Node(5,Un_node11,Un_node17)
+Un_Symmetric_Head = Node(10,Un_node9,Un_node18)
+print("The Tree is the symmetric Tree? {}".format(Check_Symmetric_Tree(Un_Symmetric_Head)))
+
+#Gas Station Travel Question
+print("------------------Gas Station Travel-----------------------------")
+gas = [1,5,3,3,5,3,1,3,4,5]
+cost = [5,2,2,8,2,4,2,5,1,2]
+def can_travel(gas,cost,start):
+    n = len(gas)
+    remaining = 0
+    i = start
+    started = False
+    while i!=start or not started:
+        # Start the loop and stoped when back to start point
+        started = True
+        remaining = remaining + gas[i]-cost[i]
+        if remaining<0:
+            return False
+        # need to conside the cycle, so that why the i should be (i=1)%n
+        i = (i+1)%n
+    return True
+def gas_station_travel(gas,cost):
+    for i in range(len(gas)):
+        if can_travel(gas,cost,i):
+            return i
+    return -1
+print("The Start Gas Station should be {}".format(gas_station_travel(gas,cost)))
+
+def gas_station(gas,cost):
+    remaining =0
+    candidate = 0
+    for i in range(len(gas)):
+        remaining = remaining+gas[i]-cost[i]
+        if remaining<0:
+            candidate = i+1
+            remaining = 0
+    prev_remaining = sum(gas[:candidate])-sum(cost[:candidate])
+    # No candicate since the cadicate is the end of the list
+    if candidate == len(gas):
+        return -1
+    # Cannot travel the second part of the cycle
+    elif candidate<len(gas) and remaining+prev_remaining<0:
+        return -1
+    else:
+        return candidate
+print("The Start Gas Station should be {}".format(gas_station_travel(gas,cost)))
