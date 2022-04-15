04e41d0bc30b3fb0f08e9dc0250d4656b7ef3c74
RZ23 <rz_jiang@yahoo.com>
2022-04-14 22:23:01 -0600

    39. Combination Sum
    39. Combination Sum

--- Leetcode.py
+++ Leetcode.py
@@ -1359,3 +1359,23 @@ def countAndSay(n):
     return s
 for i in range(1,11):
     print(countAndSay(i))
+
+print("---------------------39. Combination Sum-------------------------")
+def combinationSum(candidates, target):
+    result = []
+
+    def dfs(i,cur,total):
+        if total==target:
+            result.append(cur.copy())
+            return
+        if i>=len(candidates) or total>target:
+            return
+        cur.append(candidates[i])
+        dfs(i,cur,total+candidates[i])
+        cur.pop()
+        dfs(i+1,cur,total)
+    dfs(0,[],0)
+    return result
+test_list = [[[2,3,6,7],7],[[2,3,5],8],[[2],1]]
+for candidates,target in test_list:
+    print("the combination of {} to get target number {} are {}".format(candidates,target,combinationSum(candidates,target)))
