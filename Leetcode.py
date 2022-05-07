5560a6dbea799b91163612b1ab0931c2091cc14e
RZ23 <rz_jiang@yahoo.com>
2022-05-07 00:00:39 -0600

    133. Clone Graph
    133. Clone Graph

--- Leetcode.py
+++ Leetcode.py
@@ -29,6 +29,48 @@ def generate_tree_from_list(root):
             if right_child<len(node_list):
                 node_list[i].right = node_list[right_child]
     return node_list
+class Graph_Node():
+    def __init__(self,value = 0,neighbors = None):
+        self.value = value
+        self.neighbors = neighbors if neighbors is not None else []
+def display_graph(node):
+    if node is None:
+        return []
+    display_order = {}
+    q= deque()
+    q.append(node)
+    visit = set()
+    visit.add(node)
+    result = []
+    while len(q)>0:
+        cur_node =q.popleft()
+        print("{}".format(cur_node.value),end = ":")
+        sub_result = []
+        for neighbor in cur_node.neighbors:
+            if neighbor not in visit:
+                q.append(neighbor)
+                visit.add(neighbor)
+            print(neighbor.value,end = ",")
+            sub_result.append(neighbor.value)
+        print()
+        # result.append(sub_result)
+        display_order[cur_node.value] = sub_result
+    for i in range(1,len(display_order.keys())+1):
+        result.append(display_order[i])
+    return result
+def generate_graph(graph_node_list):
+    if len(graph_node_list)==0:
+        return None
+    graph = []
+    graph_map = {}
+    for i in range(len(graph_node_list)):
+        index = str(i+1)
+        graph.append(Graph_Node(i+1))
+        graph_map[index] = graph[i]
+    for i in range(len(graph)):
+        for adj in graph_node_list[i]:
+            graph[i].neighbors.append(graph_map[str(adj)])
+    return graph[0]
 #Roman to Int
 print("---------------------Translate Roman Numbers to Integer Number -------------------------")
 def romanToInt(s):
@@ -2977,3 +3019,27 @@ def uniquePaths(m,n):
 test_case = [[3,7],[3,2],[1,5],[5,1]]
 for m,n in test_case:
     print("There are {} ways to get from left-top to right-bottom in the {} * {} grid".format(uniquePaths(m,n),m,n))
+print("---------------------133. Clone Graph -------------------------")
+def cloneGraph(node):
+    '''
+    Using hashtable to store the map of old and new Node
+    using the DFS (Deep First Search) to find the adjacency node
+    '''
+    hash_map_new_old_map = {}
+    def dfs(node):
+        if node in hash_map_new_old_map:
+            return hash_map_new_old_map[node]
+        # copy the value of the old node
+        copy = Graph_Node(node.value)
+        hash_map_new_old_map[node] = copy
+        for neighbor in node.neighbors:
+            copy.neighbors.append(dfs(neighbor))
+        return copy
+    return dfs(node) if node else None
+test_case = [[[2,4],[1,3],[2,4],[1,3]],[[]],[]]
+for adjList in test_case:
+    node = generate_graph(adjList)
+    print("The Original Graph:")
+    print(display_graph(node))
+    print("The Result after Clone:")
+    print(display_graph(cloneGraph(node)))
