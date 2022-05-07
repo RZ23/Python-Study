f430c3eeb0788714964ac078d0be949658ef2830
RZ23 <rz_jiang@yahoo.com>
2022-05-06 18:05:15 -0600

    362. Unique Paths
    362. Unique Paths

--- Leetcode.py
+++ Leetcode.py
@@ -2965,3 +2965,15 @@ test_case = [[3,2,3,None,3,None,1],[3,4,5,1,3,None,1]]
 for node_list in test_case:
     tree = generate_tree_from_list(node_list)
     print("The Map of Houses is {} and The Maximum Profit is {}".format(tree[0],rob_binary_tree(tree[0])))
+
+print("---------------------362. Unique Paths-------------------------")
+def uniquePaths(m,n):
+    matrix = [[1 for _ in range(n)] for _ in range(m)]
+    for row in range(1,m):
+        for col in range(1,n):
+            matrix[row][col] =matrix[row][col-1]+matrix[row-1][col]
+    print_matrix(matrix)
+    return matrix[m-1][n-1]
+test_case = [[3,7],[3,2],[1,5],[5,1]]
+for m,n in test_case:
+    print("There are {} ways to get from left-top to right-bottom in the {} * {} grid".format(uniquePaths(m,n),m,n))
