f8a19d7170293affa85f9b64a610832310450595
RZ23 <rz_jiang@yahoo.com>
2022-05-09 22:30:58 -0600

    323. Number of Connected Components
    323. Number of Connected Components in an Undirected Graph

--- Leetcode.py
+++ Leetcode.py
@@ -3278,3 +3278,35 @@ def validTree(n,edges):
 n=5
 edges = [[0,1],[0,2],[0,3],[1,4]]
 print("based on the edges {}, these {} node(s) is the tree: {}".format(edges, n, validTree(n,edges)))
+print("---------------------323. Number of Connected Components in an Undirected Graph-------------------------")
+print("***** Method One: Using rank and Union Find Method *****")
+def countComponent(n,edges):
+    par = [i for i in range(n)]
+    rank = [1]*n
+    # find item's parent node
+    def find(n1):
+        res = n1
+        while res!=par[res]:
+            par[res] = par[par[res]]
+            res = par[res]
+        return res
+    # find there is the connection of two nodes
+    def union(n1,n2):
+        p1,p2 = find(n1),find(n2)
+        # if two nodes have the same parent node
+        if p1==p2:
+            return 0
+        if rank[p2]>rank[p1]:
+            par[p1] = p2
+            rank[p2]=rank[p2]+rank[p1]
+        else:
+            par[p2] = p1
+            rank[p1] = rank[p1]+rank[p2]
+        return 1
+    res = n
+    for n1,n2 in edges:
+        res = res-union(n1,n2)
+    return res
+test_case = [[5,[[0,1],[1,2],[3,4]]]]
+for n,edges in test_case:
+    print("In this {} nodes graph with {} edges, there are {} Connected Components".format(n,edges, countComponent(n,edges)))
