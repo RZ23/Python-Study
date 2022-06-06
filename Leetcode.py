7df602f6537b01a86c7077d06fa7163d2daa80db
RZ23 <rz_jiang@yahoo.com>
2022-06-05 20:19:31 -0600

    131. Palindrome Partitioning
    131. Palindrome Partitioning

--- Leetcode.py
+++ Leetcode.py
@@ -4620,3 +4620,31 @@ def findRepeatedDnaSequences(s):
 test_case = ["AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT","AAAAAAAAAAAAA"]
 for s in test_case:
     print(f"The repeat DNA sequences in {s} is {findRepeatedDnaSequences(s)}")
+print("---------------------131. Palindrome Partitioning-------------------------")
+'''
+Using Backtracking Algorithm. scan the characters one by one. if it meet the requirement, append to temp list and 
+continue to scan next character
+'''
+def partition(s):
+    result = []
+    part=[]
+    def isPali(s,l,r):
+        while l<r:
+            if s[l]!=s[r]:
+                return False
+            l,r = l+1,r-1
+        return True
+    def dfs(i):
+        if i>=len(s):
+            result.append(part.copy())
+            return
+        for j in range(i,len(s)):
+            if isPali(s,i,j):
+                part.append(s[i:j+1])
+                dfs(j+1)
+                part.pop()
+    dfs(0)
+    return result
+test_case = ["aab","a"]
+for s in test_case:
+    print(f"The Palindrome Partitioning in \'{s}\'is {partition(s)}")
