6fce3abee7a496b567e1ab9047bf799f43423997
RZ23 <rz_jiang@yahoo.com>
2022-06-06 21:07:56 -0600

    1980. Find Unique Binary String
    1980. Find Unique Binary String

--- Backtracking.py
+++ Backtracking.py
@@ -21,3 +21,19 @@ def splitString(s):
 test_case= ["1234","050043","9080701"]
 for s in test_case:
     print(f"The String '{s}' can be split into substring in descent order and the different is one: {splitString(s)}")
+print("---------------------1980. Find Unique Binary String-------------------------")
+def findDifferentBinaryString(nums):
+    strSet = { s for s in nums}
+    def backtracking(i,cur):
+        if i==len(nums):
+            res="".join(cur)
+            return None if res in strSet else res
+        res = backtracking(i+1,cur)
+        if res:return res
+        cur[i]="1"
+        res= backtracking(i+1,cur)
+        if res:return res
+    return backtracking(0,["0" for s in nums])
+test_case = [["01","10"],["00","01"],["111","011","001"]]
+for nums in test_case:
+    print(f"The Different Binary String without {nums} is {findDifferentBinaryString(nums)}")
