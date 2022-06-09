37482d2dad829b2e13906839d74a25333faee011
RZ23 <rz_jiang@yahoo.com>
2022-06-08 21:52:28 -0600

    93. Restore IP Addresses
    93. Restore IP Addresses

--- Backtracking.py
+++ Backtracking.py
@@ -67,3 +67,30 @@ def maxLength(arr):
 test_case = [["un","iq","ue"],["cha","r","act","ers"],["abcdefghijklmnopqrstuvwxyz"]]
 for arr in test_case:
     print(f"The maximum length of Concatenated String with Unique Characters ins '{arr}' is {maxLength(arr)}")
+print("---------------------93. Restore IP Addresses-------------------------")
+def restoreIpAddresses(s):
+    result = []
+    if len(s)>12:
+        return result
+    def backtracking(i,dots,curIP):
+        # 4 dots means meet the requirement of the IP address
+        # and variable i equals the length of s means scan all the characters
+        # so append it to result, except the last character in the curIP since it is "."
+        if dots==4 and i==len(s):
+            result.append(curIP[:-1])
+            return
+        # if dots greater than 4, means it exceeds the requirement of the IP address
+        # then return nothing (back)
+        if dots>4:
+            return
+        # start the backtracking, each loop is 3 characters or if it exceeds range, so
+        #using the min(i+3,len(s))
+        for j in range(i,min(i+3,len(s))):
+            # make sure the numer is less than 255 or except the 0, there is no 0 leading numbers
+            if int(s[i:j+1])<=255 and (i==j or s[i]!="0"):
+                backtracking(j+1,dots+1,curIP+s[i:j+1]+".")
+    backtracking(0,0,"")
+    return result
+test_case = ["25525511135","0000","101023"]
+for s in test_case:
+    print(f"The all possible valid IP addresses that can be formed by inserting dots into '{s}' are {restoreIpAddresses(s)}")
