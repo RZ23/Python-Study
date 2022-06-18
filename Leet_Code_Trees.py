6d53755ef8c2b0388d4456f22f70ca12e5da64f7
RZ23 <rz_jiang@yahoo.com>
2022-06-17 20:10:07 -0600

    105. Construct Binary Tree from Preorder and Inorder Traversal
    105. Construct Binary Tree from Preorder and Inorder Traversal

--- Leet_Code_Trees.py
+++ Leet_Code_Trees.py
@@ -377,3 +377,18 @@ for node_list in test_case:
     root = generate_tree_from_list(node_list)
     print(root)
     print(f"The Level order is {levelOrder(root)}")
+print("---------------------105. Construct Binary Tree from Preorder and Inorder Traversal-------------------------")
+def buildTree(preorder, inorder):
+    if not preorder or not inorder:
+        return None
+    root = TreeNode(preorder[0])
+    ind = inorder.index(preorder[0])
+    root.left = buildTree(preorder[1:ind+1],inorder[:ind])
+    root.right = buildTree(preorder[ind+1:],inorder[ind+1:])
+    return root
+test_case = [[[3,9,20,15,7], [9,3,15,20,7]],[[-1],[-1]]]
+for preorder,inorder in test_case:
+    root = buildTree(preorder,inorder)
+    print(f"The tree based on preorder {preorder} and inorder {inorder} is :")
+    print(root)
+
