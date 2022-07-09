c835cbfa53819ab5fb7e6966d1c3385a853f91e0
RZ23 <rz_jiang@yahoo.com>
2022-07-08 22:11:13 -0600

    200. Number of Islands
    200. Number of Islands

--- Leet_Code_Graph.py
+++ Leet_Code_Graph.py
@@ -1,3 +1,11 @@
+def print_matrix(dp):
+    for i in range(len(dp)):
+        for j in range(len(dp[0])):
+            if isinstance(dp[i][j],int):
+                print(format(dp[i][j],'02d'), end="|")
+            else:
+                print(dp[i][j], end="|")
+        print()
 print("---------------------207. Course Schedule-------------------------")
 def canFinish(numCourses,prerequisites):
     course_adj = {i:[] for i in range(numCourses)}
@@ -64,3 +72,42 @@ def findOrder(numCourses,prerequisites):
 test_case =[[2,[[1,0]]],[4,[[1,0],[2,0],[3,1],[3,2]]],[1,[]],[2,[[1,0],[0,1]]]]
 for numCourses, prerequisites in test_case:
     print(f"The order of {numCourses} course(s) with prerequisites {prerequisites} is {findOrder(numCourses,prerequisites)}")
+print("---------------------200. Number of Islands-------------------------")
+def numIslands(grid):
+    row = len(grid)
+    col = len(grid[0])
+    def dfs(r,c):
+        if (r<0 or c<0 or r>=row or c>=col or grid[r][c]!="1"):
+            return
+        grid[r][c]="x"
+        dfs(r,c+1)
+        dfs(r,c-1)
+        dfs(r+1,c)
+        dfs(r-1,c)
+    if len(grid)==0 or len(grid[0])==0:
+        return 0
+    count = 0
+    for r in range(row):
+        for c in range(col):
+            if grid[r][c]=="1":
+                count = count+1
+                dfs(r,c)
+    print("The visited map is ")
+    print_matrix(grid)
+    return count
+test_case =[[
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+],
+[
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]]
+for grid in test_case:
+    print(f"The Map is ")
+    print_matrix(grid)
+    print(f"There is(are) {numIslands(grid)} island(s) in this map")
