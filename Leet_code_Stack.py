8701e480e7eebf1b55cc92b26fc71dce50f85057
RZ23 <rz_jiang@yahoo.com>
2022-08-02 17:12:34 -0600

    71. Simplify Path
    71. Simplify Path

--- Leet_code_Stack.py
+++ Leet_code_Stack.py
@@ -18,3 +18,22 @@ test_case = ["()","()[]{}","(]"]
 for i,s in enumerate(test_case):
     print(f"Test Case {i+1}:")
     print(f"The Parentheses sequence '{s}' is valid: {isValid(s)} ")
+print("---------------------71. Simplify Path-------------------------")
+def simplifyPath(path):
+    stack = []
+    cur = ""
+    for c in path+"/":
+        if c == "/":
+            if cur == "..":
+                if len(stack)>0:
+                    stack.pop()
+            elif cur != "" and cur != ".":
+                stack.append(cur)
+            cur = ""
+        else:
+            cur = cur+c
+    return "/"+"/".join(stack)
+test_case = ["/home/","/../","/home//foo/"]
+for i, path in enumerate(test_case):
+    print(f"Test Case {i+1}:")
+    print(f"The Canonical path of Absolute path {path} is {simplifyPath(path)}")
