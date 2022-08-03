97104db8f24256aad632f37ce7c278361aacb199
RZ23 <rz_jiang@yahoo.com>
2022-08-03 17:12:09 -0600

    84. Largest Rectangle in Histogram
    84. Largest Rectangle in Histogram

--- Leet_code_Stack.py
+++ Leet_code_Stack.py
@@ -37,3 +37,25 @@ test_case = ["/home/","/../","/home//foo/"]
 for i, path in enumerate(test_case):
     print(f"Test Case {i+1}:")
     print(f"The Canonical path of Absolute path {path} is {simplifyPath(path)}")
+print("---------------------84. Largest Rectangle in Histogram-------------------------")
+'''
+Time Complexity 0(n)
+Space Complexity 0(n)
+'''
+def largestRectangleArea(heights):
+   maxArea = 0
+   stack = [] # (index,height)
+   for i, h in enumerate(heights):
+       start = i
+       while stack and stack[-1][1]>h:
+           index,height = stack.pop()
+           maxArea = max(maxArea,height*(i-index))
+           start = index
+       stack.append((start,h))
+   for i,h in stack:
+       maxArea = max(maxArea,h*(len(heights)-i))
+   return maxArea
+test_case = [[2,1,5,6,2,3],[2,4]]
+for i,heights in enumerate(test_case):
+    print(f"Test Case {i+1}:Heights List is {heights}")
+    print(f"The area of the largest rectangle in the histogram is {largestRectangleArea(heights)}")
