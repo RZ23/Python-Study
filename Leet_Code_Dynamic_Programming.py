6b2ea6086f6340c62ff9754c7767e4dfc8ef1918
RZ23 <rz_jiang@yahoo.com>
2022-08-20 21:19:15 -0600

    70. Climbing Stairs
    70. Climbing Stairs

--- Leet_Code_Dynamic_Programming.py
+++ Leet_Code_Dynamic_Programming.py
@@ -17,3 +17,35 @@ for i,test in enumerate(test_case):
     print(f"Test Case {i+1}:")
     print(f"There are {findTargetSumWays(test[0],test[1])} ways tp get the sum {test[1]}"
           f" from the given list {test[0]}")
+print("---------------------70. Climbing Stairs -------------------------")
+print("***** Method One: iteration *****")
+def climbStairs(n):
+    if n==1:
+        return 1
+    if n==2:
+        return 2
+    else:
+        return climbStairs(n-1)+climbStairs(n-2)
+for i in range(1,11):
+    print(f"For {i} stairs floor, there are {climbStairs(i)} to reach the top")
+print("***** Method Two: Dynamic Programming *****")
+def climbStairs(n):
+    if n<3:
+        return n
+    dp = [0]*(n+1)
+    dp[2],dp[1]=2,1
+    for i in range(3,n+1):
+        dp[i] = dp[i-1]+dp[i-2]
+    return dp[n]
+for i in range(1,11):
+    print(f"For {i} stairs floor, there are {climbStairs(i)} to reach the top")
+print("***** Method Two: Dynamic Programming II *****")
+def climbStairs(n):
+    one_step,two_step = 1,1
+    for i in range(n-1):
+        temp = one_step
+        one_step = one_step+two_step
+        two_step = temp
+    return one_step
+for i in range(1,11):
+    print(f"For {i} stairs floor, there are {climbStairs(i)} to reach the top")
