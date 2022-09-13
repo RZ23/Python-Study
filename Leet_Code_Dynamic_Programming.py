0181f99fa706cbeb946a80af873462b65589eb58
RZ23 <rz_jiang@yahoo.com>
2022-09-12 18:02:18 -0600

    1866. Number of Ways to Rearrange Sticks With K Sticks Visible
    1866. Number of Ways to Rearrange Sticks With K Sticks Visible

--- Leet_Code_Dynamic_Programming.py
+++ Leet_Code_Dynamic_Programming.py
@@ -788,3 +788,29 @@ def maxCoins(nums):
 for i, nums in enumerate(test_case):
     print(f"Based on the nums {nums}, the maximum"
           f"coins to burst balloons is {maxCoins(nums)}")
+print("-------------------- 1866. Number of Ways to Rearrange Sticks With K Sticks Visible -------------------------")
+print("***** Method One: Dynamic Programming - Recursive")
+def rearrangeSticks(n,k):
+    dp = {}
+    def dfs(N, K):
+        if N == K:
+            return 1
+        if N == 0 or K == 0:
+            return 0
+        if (N, K) in dp:
+            return dp[(N, K)]
+        dp[(N, K)] = ((dfs(N - 1, K - 1) + (N - 1) * dfs(N - 1, K))) % (10 ** 9 + 7)
+        return dp[(N, K)]
+    return dfs(n, k)
+test_case = [[3,2],[5,5],[20,11]]
+for i, test in enumerate(test_case):
+    print(f"Test Case {i+1}: There is(are) {rearrangeSticks(test[0],test[1])} ways for {test[0]} sticks and {test[1]} visiable")
+print("***** Method Two: Dynamic Programming - Cache")
+def rearrangeSticks(n,k):
+    dp = {(1,1):1}
+    for N in range(2,n+1):
+        for K in range(1,k+1):
+            dp[(N,K)] = (dp.get((N-1,K-1),0)+(N-1)*dp.get((N-1,K),0))
+    return dp[(n,k)]% (10**9+7)
+for i, test in enumerate(test_case):
+    print(f"Test Case {i+1}: There is(are) {rearrangeSticks(test[0],test[1])} ways for {test[0]} sticks and {test[1]} visiable")
