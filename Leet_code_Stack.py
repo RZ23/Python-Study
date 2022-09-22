cda3c318bc985d6a696c1b0e1d4eac069437fb80
RZ23 <rz_jiang@yahoo.com>
2022-09-21 21:25:35 -0600

    394. Decode String
    394. Decode String

--- Leet_code_Stack.py
+++ Leet_code_Stack.py
@@ -574,3 +574,22 @@ def find132pattern(nums):
 for i,nums in enumerate(test_case):
     print(f"Test Case {i+1}:")
     print(f"There is the subsquence in the {nums} meet the 132 pattern:{find132pattern(nums)}")
+print("---------------------394. Decode String-------------------------")
+def decodeString(s):
+    stack = []
+    for i in range(len(s)):
+        if s[i]!="]":
+            stack.append(s[i])
+        else:
+            substr = ""
+            while stack[-1]!="[":
+                substr = stack.pop()+substr
+            stack.pop()
+            k = ""
+            while stack and stack[-1].isdigit():
+                k = stack.pop()+k
+            stack.append(int(k)*substr)
+    return "".join(stack)
+test_case = ["3[a]2[bc]","3[a2[c]]","2[abc]3[cd]ef"]
+for i,s in enumerate(test_case):
+    print(f"Test Case {i+1}: The result of decoding '{s} is {decodeString(s)}'")
